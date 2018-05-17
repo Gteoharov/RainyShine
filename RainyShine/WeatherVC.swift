4e2165fa1a04367337e89e8c6c244ff7bea6bff6
Gteoharov <Gteoharov@users.noreply.github.com>
2018-05-17 17:46:53 +0300

    Cocoapods and UI

---
+++ RainyShine/WeatherVC.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherVC.swift
+//  RainyShine
+//
+//  Created by Georgi Teoharov on 17.05.18.
+//  Copyright © 2018 Georgi Teoharov. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+}
+
